bef8ae3ed8bd31395ea5a298a5c0adc2bd8ec894
amal-kadri <98130059+amal-kadri@users.noreply.github.com>
2022-05-09 15:00:45 -0500

    misc data cleaning

---
+++ code/data_clean.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+library(readr)
+
+#read in data
+CDC_20to64_State <- read_csv("data/CDC_Mortality_State_ICD_20To64_RACE_YEAR.csv")
+names(CDC_20to64_State) = make.names(colnames(CDC_20to64_State))
+
+#preliminary cleaning strings and oddities
+sapply(CDC_Corrected, class)
+' (Unreliable)'
+CDC_Corrected = CDC_20to64_State %>%
+  mutate(Crude.Rate =  ifelse(
+    substr(Crude.Rate, nchar(Crude.Rate)-12, nchar(Crude.Rate)) == ' (Unreliable)',
+    (substr(Crude.Rate, 1, nchar(Crude.Rate)-12)),(Crude.Rate)
+  )) %>%
+  mutate(Crude.Rate = as.numeric(Crude.Rate)) %>%
+  mutate(
+    X..of.Total.Deaths = substr(X..of.Total.Deaths,1,nchar(X..of.Total.Deaths)-1)
+  ) %>%
+  mutate(X..of.Total.Deaths = as.numeric(X..of.Total.Deaths))
+ 
+  
+
