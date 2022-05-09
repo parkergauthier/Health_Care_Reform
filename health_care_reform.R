4c0543aff0eec8d77a2d9d991b04c2c8d0dacaf6
Jirapat <jirapat@wireless-10-147-61-14.public.utexas.edu>
2022-05-09 18:34:54 -0500

    data for 56 states

--- health_care_reform.R
+++ health_care_reform.R
@@ -4,5 +4,6 @@ mortality_2 <- read_tsv("https://raw.githubusercontent.com/parkergauthier/Health
 mortality_3 <- read_tsv("https://raw.githubusercontent.com/parkergauthier/Health_Care_Reform/main/Compressed_Mortality31to45.txt")
 mortality_4 <- read_tsv("https://raw.githubusercontent.com/parkergauthier/Health_Care_Reform/main/Compressed_Mortality46to56.txt")
 
-df <- rbind(mortality_1, mortality_2, mortality_3, mortality_4)
-df <- select(df, -c(Notes)) %>% drop_na()
+df <- rbind(mortality_1, mortality_2, mortality_3, mortality_4) %>%
+    select(df, -c(Notes)) %>% 
+    drop_na()
