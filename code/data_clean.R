bdd7bd1ed5e2ecc2f691b95e0a5ebc596ca48b95
akemp18 <51224047+akemp18@users.noreply.github.com>
2022-05-10 11:22:46 -0500

    all sc data cleaned

--- code/data_clean.R
+++ code/data_clean.R
@@ -21,6 +21,25 @@ CDC_Corrected = CDC_20to64_State %>%
   filter(Year >= 2000)
 
 ## FEATURES FOR SC 
+# state population (Census) - for scaling
+state_pop = read.csv("data/state-pops-2000-2010.csv") %>%
+  data.frame %>%
+  pivot_longer(X2000:X2010) %>%
+  mutate(name = recode(name,
+    X2000 = "2000",
+    X2001 = "2001",
+    X2002 = "2002",
+    X2003 = "2003",
+    X2004 = "2004",
+    X2005 = "2005",
+    X2006 = "2006",
+    X2007 = "2007",
+    X2008 = "2008",
+    X2009 = "2009",
+    X2010 = "2010",
+  ))
+colnames(state_pop) = c("State", "Year", "Population")
+
 # state level unemployment 
 unemp = read.csv("data/state-unemployment.csv") %>% 
   pivot_longer(cols = X2000:X2016) %>%
@@ -29,16 +48,26 @@ unemp = read.csv("data/state-unemployment.csv") %>%
 colnames(unemp) = c("State", "Year", "Unemp_Rate")
 
 # state age-sex data 2000 - 2010 (Census intercensal estimates)
-state_agesex_2010 = read.csv("data/state-agesex-2000-2010.csv") %>% data.frame
+state_agesex = read.csv("data/state-agesex-2000-2010.csv") %>% data.frame
+state_agesexpop = merge(state_agesex, state_pop)
+state_agesexpop[,3:23] = sapply(state_agesexpop[,3:23], as.numeric)
+state_agesex_clean = state_agesexpop %>%
+  mutate(Age_20to34 = (X.20.to.24.years + X.25.to.29.years + X.30.to.34.years) / Population * 100,
+         Age_35to44 = (X.35.to.39.years + X.40.to.44.years) / Population * 100,
+         Age_45to54 = (X.45.to.49.years + X.50.to.54.years) / Population * 100,
+         Age_55to64 = (X.55.to.59.years + X.60.to.64.years) / Population * 100,
+         Male_pct = MALE / Population * 100,
+         Female_pct = FEMALE / Population * 100) %>%
+  select(State, Year, Age_20to34, Age_35to44, Age_45to54, Age_55to64, Male_pct, Female_pct)
 
-# state demos racee 2000 - 2010 (Census intercensal)
-state_race_2010 = read.csv("data/state-race-2000-2010.csv") %>% 
+# state demos race 2000 - 2010 (Census intercensal)
+state_race = read.csv("data/state-race-2000-2010.csv") %>% 
   data.frame %>%
   filter(NAME != "United States" & RACE != 0) %>% 
   select(-STATE)
-colnames(state_race_2010) = c("State", "Race", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010")
+colnames(state_race) = c("State", "Race", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010")
 
-state_race_2010_long = state_race_2010 %>% pivot_longer("2000":"2010") %>%
+state_race_clean = state_race %>% pivot_longer("2000":"2010") %>%
   pivot_wider(names_from = Race, names_prefix = "Race_", values_from = "value") %>%
   rename(
     Year = name,
@@ -47,11 +76,49 @@ state_race_2010_long = state_race_2010 %>% pivot_longer("2000":"2010") %>%
     Race_AmInd = Race_3,
     Race_Asian = Race_4,
     Race_PacIsl = Race_5,
-    Race_Multi = Race_6
-  )
+    Race_Multi = Race_6) %>% 
+  merge(state_pop) %>%
+  mutate(
+    Race_White = Race_White / Population * 100,
+    Race_Black = Race_Black / Population * 100,
+    Race_AmInd = Race_AmInd / Population * 100,
+    Race_Asian = Race_Asian / Population * 100,
+    Race_PacIsl = Race_PacIsl / Population * 100,
+    Race_Multi = Race_Multi / Population * 100,) %>% 
+  select(-Population)
 
-c1 = merge(unemp, state_agesex_2010)
-c2 = merge(c1, state_race_2010_long)
+state_hhinc = read.csv("data/state-hhinc-2000-2010.csv") %>%
+  data.frame %>%
+  filter(State != "United States") %>%
+  pivot_longer(X2000:X2010) %>%
+  mutate(name = recode(name,
+                       X2000 = "2000",
+                       X2001 = "2001",
+                       X2002 = "2002",
+                       X2003 = "2003",
+                       X2004 = "2004",
+                       X2005 = "2005",
+                       X2006 = "2006",
+                       X2007 = "2007",
+                       X2008 = "2008",
+                       X2009 = "2009",
+                       X2010 = "2010",
+  ))
+colnames(state_hhinc) = c("State", "Year", "HH_Income")
+
+# state poverty rate (Census) 
+state_poverty = read.csv("data/state-poverty-2000-2010.csv")
+
+# state uninsured rate for population under 65 years (CPS)
+state_uninsured = read.csv("data/state-uninsured.csv") 
+
+# merge all data
+c1 = merge(unemp, state_agesex_clean)
+c2 = merge(c1, state_race_clean)
+c3 = merge(c2, state_hhinc)
+c4 = merge(c3, state_poverty)
+c5 = merge(c4, state_uninsured)
+
 
 
 df = CDC_Corrected %>%
